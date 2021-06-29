e627826f1d4779d0697835b393b3d1218f688591
Kevin Stradtman <kevin.stradtman@gmail.com>
2021-06-29 12:18:33 -0400

    Created the models

---
+++ src/main/java/com/stradtman/springpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.stradtman.springpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/stradtman/springpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.stradtman.springpetclinic.model;
+
+public class Vet extends Person {
+
+}
